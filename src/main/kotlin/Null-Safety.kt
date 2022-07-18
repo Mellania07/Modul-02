73899a246b38cd5f1b0708e598ff7fff8abab933
acer <mellaniabks@gmail.com>
2022-07-18 11:19:58 +0700

    contoh pemberian nilai null

---
+++ src/main/kotlin/Null-Safety.kt
@@ -0,0 +1,8 @@
+//contoh pemberian nilai null yang diizinkan
+
+fun main(args: Array<String>){
+    //Nullable types
+    var maybeNumber: Int? = 15
+    maybeNumber = null
+    println("maybeNumber: $maybeNumber ${maybeNumber?.hashCode()}")
+}
