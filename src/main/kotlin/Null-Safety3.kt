d3175066807aa97fddf0aa9652d63c82c975362a
acer <mellaniabks@gmail.com>
2022-07-18 11:29:04 +0700

    pemanggilan yg aman

---
+++ src/main/kotlin/Null-Safety3.kt
@@ -0,0 +1,11 @@
+//Pemanggilan yang aman (Safe Calls)
+
+fun main(args: Array<String>) {
+    //Safe calling
+    //?.let
+    var maybeNumber: Int? = 15
+    maybeNumber?.let { println(it) }
+
+    //The ?. chaining
+    //student?.teacher?.supervisor?.name = "Seymour Skinner"
+}
